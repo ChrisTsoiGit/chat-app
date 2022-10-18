e1498d2ca097e1a764e2d6c4ed021770330d9f05
Qingying Meng <qmeng222@gmail.com>
2022-10-17 18:55:33 -0700

    temp

--- ghi/src/store/store.js
+++ ghi/src/store/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersApi } from "./usersApi";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 // create a Redux store:
 export const store = configureStore({
   reducer: {
     [usersApi.reducerPath]: usersApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(usersApi.middleware),
 });
 
-export default store;
+// export default store;
+setupListeners(store.dispatch);
